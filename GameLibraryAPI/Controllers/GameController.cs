0ceac819f8f47e394dcb5e0fa9a8ca84e73f6fb3
AxelL <AxelL@AXEL>
2022-06-19 19:24:08 +0200

    Add project files.

---
+++ GameLibraryAPI/Controllers/GameController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GameLibraryAPI.Controllers
+{
+    [Route("api/[controller")]
+    [ApiController]
+    public class GameController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ GameLibraryAPI/Game.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace GameLibraryAPI
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Publisher { get; set; }
+        public enum _Genre { Platformer, Horror, Ppg, Survival, Vr, Roguelike, Adventure, Exploration}
+        public enum _Completion { NotStarted, OnGoing, Finished}
+
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public _Genre Genre { get; set; }
+
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public _Completion Completion { get; set; }
+    }
+}
